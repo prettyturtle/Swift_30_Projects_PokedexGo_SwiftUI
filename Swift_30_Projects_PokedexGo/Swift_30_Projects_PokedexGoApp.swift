7fa516f0fb4389069b2bc8e28cf3226ad14adce0
prettyturtle <unifconti@gmail.com>
2023-05-07 02:02:37 +0900

    Initial Commit

---
+++ Swift_30_Projects_PokedexGo/Swift_30_Projects_PokedexGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_PokedexGoApp.swift
+//  Swift_30_Projects_PokedexGo
+//
+//  Created by yc on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_PokedexGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
